8ea1c4a709d15cea227c50a45b04dccf37fe901a
Balaji Krishnan <balaji.krishnan@microsoft.com>
2017-08-02 15:02:04 -0700

    Adding documentation

--- sample/client/sample.ts
+++ sample/client/sample.ts
@@ -1,12 +1,12 @@
 import { AuthenticationManager } from "../../lib/AuthenticationManager";
 
 const config: adal.Config = {
-  clientId: "4e577d28-b99d-4b38-829a-1adc38b0fab5",
-  tenant: "72f988bf-86f1-41af-91ab-2d7cd011db47",
+  clientId: "",
+  tenant: "",
   popUp: false
 };
 
 const authManager = new AuthenticationManager(config);
 authManager.login();
-
-document.write("Hey there sample js-auth!!")
+authManager.handleWindowCallback();
+document.write("Hey there sample js-auth!!");
